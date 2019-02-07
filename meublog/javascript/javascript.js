a0476461f6698bd23b243846ba39128fc6efc85f
asfss1 <asfss1@gmail.com>
2019-02-06 21:46:44 -0300

    blog

---
+++ meublog/javascript/javascript.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+function myHref()
+{
+var plus = "<font color=""#0099FF"">+</font>"
+var infoOver = "info " + plus
+document.getElementById('info').innerHTML= infoOver
+}
+</script>
