aa8b2c9c636c4a69edfea8d7f185cbe66651c7dc
augusto <tmanelotacao@gmail.com>
2019-03-16 18:48:03 -0300

    envio

--- meublog/javascript/javascript.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-function myHref()
-{
-var plus = "<font color=""#0099FF"">+</font>"
-var infoOver = "info " + plus
-document.getElementById('info').innerHTML= infoOver
-}
-</script>
